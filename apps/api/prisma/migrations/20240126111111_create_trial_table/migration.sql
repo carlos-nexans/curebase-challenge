278610a06eb181d79a5002f54f6754583c5f0a4e
Carlos Nexans <carlos.nexans@clau.com>
2025-01-27 13:44:36 -0300

    refactor: change sqlite to postgres (#7)
    * refactor: change sqlite to postgres
    
    * chore: improve tests to have database
    
    * chore: update docker compose
    
    * chore: add arg

--- apps/api/prisma/migrations/20240126111111_create_trial_table/migration.sql
+++ apps/api/prisma/migrations/20240126111111_create_trial_table/migration.sql
@@ -1,18 +1,18 @@
 -- CreateTable
 CREATE TABLE "Trial" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL PRIMARY KEY,
     "name" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Participant" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "height" REAL NOT NULL,
     "weight" REAL NOT NULL,
     "hasDiabetes" BOOLEAN NOT NULL,
     "hadCovid" BOOLEAN NOT NULL,
     "trialId" INTEGER NOT NULL,
-    "enrolledAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY ("trialId") REFERENCES "Trial"("id")
+    "enrolledAt" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Participant_trialId_fkey" FOREIGN KEY ("trialId") REFERENCES "Trial"("id")
 );
